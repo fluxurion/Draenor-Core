e7715fd8725735ecd3d1b6141a35ec307d7a4915
dark <zemetskia@gmail.com>
2023-02-04 23:08:57 +0300

    delete not need config

--- src/server/worldserver/TCSoap/TCSoap.cpp
+++ src/server/worldserver/TCSoap/TCSoap.cpp
@@ -1,10 +1,20 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-// Project-Hellscream https://hellscream.org
-// Copyright (C) 2018-2020 Project-Hellscream-6.2
-// Discord https://discord.gg/CWCF3C9
-//
-////////////////////////////////////////////////////////////////////////////////
+/*
+* Copyright (C) 2008-2020 TrinityCore <http://www.trinitycore.org/>
+* Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "TCSoap.h"
 #include "soapH.h"
@@ -109,9 +119,7 @@ int ns1__executeCommand(soap* soap, char* command, char** result)
 
     int acc = connection.pendingCommands.acquire();
     if (acc)
-    {
         TC_LOG_ERROR("server.worldserver", "TCSoap: Error while acquiring lock, acc = %i, errno = %u", acc, errno);
-    }
 
     // alright, command finished
 
